369886c52795ff13bcc306862527306c5ce80c96
Редько Никита Сергеевич <@crmpoos.ru>
2024-11-21 12:36:42 +0300

    Изменил файл конфига nginx

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-// Получаем все кнопки с классом 'toggle-button' и все блоки с классом 'collapse'
 const toggleButtons = document.querySelectorAll('.toggle-button');
 const collapseBlocks = document.querySelectorAll('.collapse');
 
-// Применяем общий обработчик для всех кнопок
 toggleButtons.forEach(button => {
     button.addEventListener('click', () => {
         const target = document.querySelector(button.getAttribute('data-bs-target'));
         
-        // Меняем текст и aria-expanded в зависимости от состояния блока
         if (target.classList.contains('show')) {
             button.textContent = 'Подробнее о проекте';
             button.setAttribute('aria-expanded', 'false');
