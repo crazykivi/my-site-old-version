fcccddb00b9192416608363f094a13811743d17b
Редько Никита Сергеевич <@crmpoos.ru>
2024-11-21 12:42:31 +0300

    Добавил удаление идентификатор при перезагрузке

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
-const toggleButtons = document.querySelectorAll('.toggle-button');
-const collapseBlocks = document.querySelectorAll('.collapse');
+const toggleButtons = document.querySelectorAll(".toggle-button");
+const collapseBlocks = document.querySelectorAll(".collapse");
 
-toggleButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const target = document.querySelector(button.getAttribute('data-bs-target'));
-        
-        if (target.classList.contains('show')) {
-            button.textContent = 'Подробнее о проекте';
-            button.setAttribute('aria-expanded', 'false');
-        } else {
-            button.textContent = 'Закрыть подробности';
-            button.setAttribute('aria-expanded', 'true');
-        }
-    });
+toggleButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const target = document.querySelector(
+      button.getAttribute("data-bs-target")
+    );
+
+    if (target.classList.contains("show")) {
+      button.textContent = "Подробнее о проекте";
+      button.setAttribute("aria-expanded", "false");
+    } else {
+      button.textContent = "Закрыть подробности";
+      button.setAttribute("aria-expanded", "true");
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.location.hash) {
+    history.replaceState(
+      null,
+      null,
+      window.location.pathname + window.location.search
+    );
+  }
 });
