30a3333929eac8b78f2faff8948a14951934cf53
Редько Никита Сергеевич <@crmpoos.ru>
2024-12-02 08:59:23 +0300

    фиксы

--- js/blog/renderRecords.js
+++ js/blog/renderRecords.js
@@ -1,5 +1,4 @@
 function renderRecords(records) {
-  const token = localStorage.getItem("token");
   const recordsList = document.getElementById("records-list");
   recordsList.innerHTML = "";
 
@@ -7,30 +6,29 @@ function renderRecords(records) {
     const recordElement = document.createElement("div");
     recordElement.className = "card mb-3";
 
-    const contentContainer = document.createElement("div");
-    contentContainer.className = "card-body";
-
-    const parser = new DOMParser();
-    const parsedContent = parser.parseFromString(record.content, "text/html");
-
-    contentContainer.appendChild(parsedContent.body);
-    recordElement.appendChild(contentContainer);
+    recordElement.innerHTML = `<div class="card-body">
+            <div class="ql-editor">${record.content}</div>
+            </div>`;
 
     if (token) {
       const deleteButton = document.createElement("button");
       deleteButton.className = "btn btn-danger";
       deleteButton.textContent = "Удалить";
       deleteButton.setAttribute("data-id", record.id);
-
       deleteButton.addEventListener("click", () => {
         deleteRecord(record.id);
       });
-
       const cardFooter = document.createElement("div");
       cardFooter.className = "card-footer";
       cardFooter.appendChild(deleteButton);
       recordElement.appendChild(cardFooter);
     }
+    /*
+    const imgTags = recordElement.querySelectorAll("img");
+
+    imgTags.forEach((img) => {
+      console.log("Image source:", img.src);
+    }); */
 
     recordsList.appendChild(recordElement);
   });
