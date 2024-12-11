900ff1a777b4daab4036f596a3f51fd9fedbbf41
Редько Никита Сергеевич <@crmpoos.ru>
2024-12-11 13:30:38 +0300

    Доработки

--- js/index/navigator.js
+++ js/index/navigator.js
@@ -1,21 +1,14 @@
-/*
-const toggleMenuButton = document.getElementById("toggleMenu");
 const sidebar = document.getElementById("sidebar");
-
-toggleMenuButton.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-});
-*/
-const toggleMenuButton = document.getElementById("toggleMenu");
-const sidebar = document.getElementById("sidebar");
-
-toggleMenuButton.addEventListener("click", (e) => {
-  e.stopPropagation();
-  sidebar.classList.toggle("active");
-});
 
 document.addEventListener("click", (e) => {
-  if (!sidebar.contains(e.target) && !toggleMenuButton.contains(e.target)) {
-    sidebar.classList.remove("active");
+  if (sidebar.contains(e.target)) {
+    return;
   }
+
+  sidebar.classList.remove("active");
+});
+
+sidebar.addEventListener("click", (e) => {
+  e.stopPropagation();
+  sidebar.classList.add("active");
 });
