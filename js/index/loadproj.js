1a24beb06ce9cf36cc3f8756bc6a10a3880bd8c8
Редько Никита Сергеевич <@crmpoos.ru>
2024-12-11 12:40:00 +0300

    Фиксы

--- js/index/loadproj.js
+++ js/index/loadproj.js
@@ -3,7 +3,6 @@ function loadProjects() {
     .then((response) => response.json())
     .then((data) => {
       renderProjects(data.projects);
-      //console.debug("Полученные проекты:", data.projects);
     })
     .catch((error) => console.error("Ошибка загрузки данных:", error));
 }
@@ -14,14 +13,16 @@ function renderProjects(projects) {
   const projectsList = document.getElementById("projects-list");
   projectsList.innerHTML = "";
 
-  projects.forEach((project) => {
+  projects.forEach((project, index) => {
     const projectElement = document.createElement("div");
-    projectElement.className = "col-md-6 col-lg-4";
+    projectElement.className = "col-md-6 col-lg-4 project-card";
 
     projectElement.innerHTML = `
       <div class="card">
         <a href="${project.project_url}">
-          <img src="${project.image_url}" class="card-img-top" alt="${project.project_name}">
+          <img src="${project.image_url}" class="card-img-top" alt="${
+      project.project_name
+    }">
         </a>
         <div class="card-body">
           <h5 class="card-title">${project.project_name}</h5>
@@ -29,8 +30,12 @@ function renderProjects(projects) {
             Фронтенд: ${project.frontend_technologies || "Не указан"}<br>
             Бекенд: ${project.backend_technologies || "Не указан"}
           </p>
-          <a href="${project.project_url}" class="btn btn-primary btn-sm">Перейти к проекту</a>
-          <button class="btn btn-link" type="button" data-bs-toggle="collapse" data-bs-target="#aboutProject${project.project_id}" aria-expanded="false" aria-controls="aboutProject">
+          <a href="${
+            project.project_url
+          }" class="btn btn-primary btn-sm">Перейти к проекту</a>
+          <button class="btn btn-link" type="button" data-bs-toggle="collapse" data-bs-target="#aboutProject${
+            project.project_id
+          }" aria-expanded="false" aria-controls="aboutProject">
             Подробнее о проекте
           </button>
           <div class="collapse" id="aboutProject${project.project_id}">
@@ -41,6 +46,9 @@ function renderProjects(projects) {
     `;
 
     projectsList.appendChild(projectElement);
+
+    setTimeout(() => {
+      projectElement.classList.add("visible");
+    }, index * 200);
   });
 }
-
